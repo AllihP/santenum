0332ea1262ebc6bf5c91f0370c7b2ce9ef500d9d
AllihP <71613954+AllihP@users.noreply.github.com>
2025-06-29 11:25:41 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# entrypoint.sh
 
-# Appliquer les migrations
-echo "Applying database migrations..."
-python manage.py migrate --noinput
-
-# Collecter les fichiers statiques
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
-
-# Exécuter le script de création de super-utilisateur
-echo "Running create_superuser.py script..."
-python create_superuser.py
-
-# Lancer Gunicorn
-echo "Starting Gunicorn server..."
+# Start Gunicorn
+echo "Starting Gunicorn..."
 gunicorn santenumerique.wsgi:application --bind 0.0.0.0:$PORT
