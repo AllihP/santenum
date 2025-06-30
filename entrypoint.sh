be68f680035738e4c4deddfe374bac217e5c9c73
AllihP <71613954+AllihP@users.noreply.github.com>
2025-06-30 10:07:04 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
-# entrypoint.sh
 
-# Start Gunicorn
+echo "Applying database migrations..."
+python manage.py migrate --noinput || { echo "Migrations failed!"; exit 1; } # Add error handling
+
+echo "Running create_superuser.py script..."
+python create_superuser.py || { echo "Superuser creation failed!"; exit 1; } # Add error handling
+
 echo "Starting Gunicorn..."
-gunicorn santenumerique.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn santenumerique.wsgi:application --bind 0.0.0.0:$PORT || { echo "Gunicorn failed to start!"; exit 1; } # Add error handling
